f41dd2b26197e53ecd0812c9cb6e1420eef7ea1f
Benjamin Ashbaugh <blucashbaugh@gmail.com>
2019-05-01 19:56:21 +0000

    restart and die commands finshed minus sudo

--- cogs/util.py
+++ cogs/util.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from discord.ext import commands
 import discord
@@ -9,6 +10,14 @@ class Util_commands(commands.Cog, name='Utility commands'):
 
     @commands.command()
     async def die(self, ctx):
-        """Kill the process"""
+        """- Kill the process."""
         await ctx.send("My websocket connection will be closed, disconnecting me from the world, before `sys.exit()` rips me from existence. I hope this is what you wanted. Goodbye.")
         sys.exit()
+
+    @commands.command()
+    async def restart(self, ctx):
+        """- Restart the process"""
+        await ctx.send("I will now kill myself and reincarnate as a (hopefully) better bot. See you in a few seconds!")
+        await self.bot.change_presence(status=discord.Status.idle)
+        #await self.bot.close()
+        os.execl(sys.executable, sys.executable, * sys.argv) 
